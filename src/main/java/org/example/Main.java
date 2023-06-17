6b8d82c912d98b1db174282874bb6150ab5d872b
Your Name <Vakalovaelbert@yandex.ru>
2023-06-17 22:07:50 +0300

    First commit, created class PasswordGenerator

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PasswordGenerator pg = new PasswordGenerator();
+        PasswordGenerator.password(11);
+    }
+}
---
+++ src/main/java/org/example/PasswordGenerator.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    private static final String passwordLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String passwordSymbols = "/?>.<,|{{[}]*()";
+    private static final String passwordNumbers = "1234567890";
+
+
+    static char[] password(int length) {
+        String values = passwordLetter + passwordSymbols + passwordNumbers;
+        Random randomSymbols = new Random();
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            password[i] = values.charAt(randomSymbols.nextInt(values.length()));
+        }
+
+        System.out.println("Generator activate, your new password is: " + password);
+        return password;
+    }
+}
